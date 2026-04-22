bd69a74ba98d857b7ce3da154315bc1626e30e3a
Ashley Husband <134344344+ANHCXI@users.noreply.github.com>
2026-04-22 16:00:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function initMap(lat, lng) {
 }
 
 function haversineMiles(lat1, lon1, lat2, lon2) {
-    const R = 3958.8; // Earth's radius in miles
+    const R = 3958.8; 
     const dLat = (lat2 - lat1) * Math.PI / 180;
     const dLon = (lon2 - lon1) * Math.PI / 180;
     
